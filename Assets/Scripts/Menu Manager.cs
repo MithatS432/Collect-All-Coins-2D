7b5dee1296c553ee5f1d10a8c673b61094f83e95
Mithat <mithatsirmali@gmail.com>
2025-09-27 18:14:07 +0300

    Main Menu is done

--- Assets/Scripts/Menu Manager.cs
+++ Assets/Scripts/Menu Manager.cs
@@ -1,16 +1,53 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject playButton;
+    public GameObject howToPlayButton;
+    public GameObject exitButton;
+
+    public GameObject panel;
+    public GameObject closePanelButton;
+    public TextMeshProUGUI titleText;
+
+    public AudioClip buttonclickSound;
+
+    public void OnPlayButtonClicked()
     {
-        
+        AudioSource.PlayClipAtPoint(buttonclickSound, Camera.main.transform.position);
+        SceneManager.LoadScene("Field1");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnHowToPlayButtonClicked()
     {
-        
+        AudioSource.PlayClipAtPoint(buttonclickSound, Camera.main.transform.position);
+        panel.SetActive(true);
+        closePanelButton.SetActive(true);
+        titleText.gameObject.SetActive(true);
+        playButton.SetActive(false);
+        howToPlayButton.SetActive(false);
+        exitButton.SetActive(false);
+    }
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+    public void OnClosePanelButtonClicked()
+    {
+        AudioSource.PlayClipAtPoint(buttonclickSound, Camera.main.transform.position);
+        panel.SetActive(false);
+        closePanelButton.SetActive(false);
+        titleText.gameObject.SetActive(false);
+        playButton.SetActive(true);
+        howToPlayButton.SetActive(true);
+        exitButton.SetActive(true);
     }
 }
