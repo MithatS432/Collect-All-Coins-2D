6f2c1d65de7b3eb7e5ad3b891327658a2f6f1d69
Mithat <mithatsirmali@gmail.com>
2025-09-29 14:31:09 +0300

    Level Design + Player Script

--- Assets/Scripts/Camera Follow.cs
+++ Assets/Scripts/Camera Follow.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public Vector3 offset = new Vector3(0, 0, -10);
+    public Vector3 offset = new Vector3(0, 3.35f, -10f);
 
     private void LateUpdate()
     {
--- Assets/Scripts/Soldier.cs
+++ Assets/Scripts/Soldier.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Soldier : MonoBehaviour
 {
+    [Header("Components")]
     private Rigidbody2D rb;
     private Animator anim;
     private SpriteRenderer sprite;
@@ -13,9 +15,17 @@ public class Soldier : MonoBehaviour
     public AudioClip coinSound;
     public AudioClip jumpSound;
 
+    [Header("State")]
     public bool isGrounded;
     private float speed = 5f;
     private float jumpForce = 7f;
+    private float xRange = -10f;
+
+    [Header("UI")]
+    public TextMeshProUGUI leftCoinsText;
+    [SerializeField] private int startCoins = 10;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -40,6 +50,7 @@ public class Soldier : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal");
         transform.position += new Vector3(x, 0, 0) * Time.deltaTime * speed;
+        anim.SetFloat("Speed", Mathf.Abs(x));
         if (x < 0)
         {
             sprite.flipX = true;
@@ -48,6 +59,10 @@ public class Soldier : MonoBehaviour
         {
             sprite.flipX = false;
         }
+        if (transform.position.x < xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
@@ -55,6 +70,8 @@ public class Soldier : MonoBehaviour
         {
             AudioSource.PlayClipAtPoint(coinSound, transform.position);
             Destroy(other.gameObject);
+            startCoins--;
+            leftCoinsText.text = "Coins Left:" + startCoins.ToString();
         }
         if (other.gameObject.CompareTag("Flag"))
         {
